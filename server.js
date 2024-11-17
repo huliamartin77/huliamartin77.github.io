6aaed4fd02f0d1cf70b1a59f9cc817fad5380795
huliamartin77 <45413133+huliamartin77@users.noreply.github.com>
2024-11-16 22:05:42 -0600

    changes removed email check for now

--- server.js
+++ server.js
@@ -192,10 +192,6 @@ if (lowerCaseMessage.includes("current projects")) {
     }
 });
 
-/*app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});*/
-
 export default app;
 
 
