dda13441c29f8cc8e2a47f17f8f0132cba6637dc
huliamartin77 <45413133+huliamartin77@users.noreply.github.com>
2024-11-16 21:27:36 -0600

    cors change headers

--- server.js
+++ server.js
@@ -12,6 +12,9 @@ app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    if (req.method === 'OPTIONS') {
+        return res.sendStatus(204);
+    }
     next();
 });
 // Serve static files (like HTML, CSS, JS) from the current directory
